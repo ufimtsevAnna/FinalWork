0443ebc92a7eb35366ca99fb8c879cda9de76947
Anna <camelot252@gmail.com>
2022-01-15 21:25:02 +0500

    Добавила метод заполнения массива рандомными числами

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void FillArray(int[] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        matr[i] = new Random().Next(1, 11);
+        Console.Write($"{matr[i]}");
+    }
+}
+int[] matrix = new int[3];
+FillArray(matrix);
